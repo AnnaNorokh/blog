b9ef94f58e38693c6f71703d111a3a32850cb48d
Anna <annanorokh@gmail.com>
2022-01-24 01:37:19 +0200

    Level 1: Get, Post, Put, Delete. Added JPA

--- src/main/java/com/luxcampus/blog/service/BlogService.java
+++ src/main/java/com/luxcampus/blog/service/BlogService.java
@@ -1,31 +1,19 @@
 package com.luxcampus.blog.service;
 
 import com.luxcampus.blog.entity.Post;
-import com.luxcampus.blog.repository.BlogRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@RequiredArgsConstructor
-@Service
-public class BlogService {
-    private final BlogRepository blogRepository;
+public interface BlogService {
 
-    public List<Post> findAll(){
+    public List<Post> getAllPosts();
 
-        return blogRepository.findAll();
-    }
+    public Post findPostById(Integer id);
 
-    public void add (Post post){
-        blogRepository.add(post);
-    }
+    public void addPost(Post post);
 
-    public void edit (Post post){
-        blogRepository.edit(post);
-    }
+    public void editPostById(Integer id, Post post);
 
-    public void delete (int id){
-        blogRepository.delete(id);
-    }
+    public void deletePostById(Integer id);
+
 }
